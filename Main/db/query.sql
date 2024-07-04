e1a0b490e25e07d7c93f9a12e7b7ff917d64e5a2
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-07-04 13:42:32 -0500

    added query.sql

---
+++ Main/db/query.sql
@@ -0,0 +1,20 @@
+SELECT 
+    department_name
+FROM department;
+
+SELECT 
+    title,
+    salary,
+    department_id
+FROM role;
+
+SELECT 
+    first_name,
+    last_name,
+    role_id,
+    manager_id
+FROM employee;
+
+ 
+
+
