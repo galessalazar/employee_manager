950fe4d7700114ede0c771f95ed86867dad5a070
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-07-04 09:09:25 -0500

    inquirer prompt added

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 
 
-// ignores username/ passwords from being published on github
+//this makes commits ignore username/ passwords from being published on github, imports the .env file
 require ('dotenv').config();
+const inquirer = require('inquirer');
 
+const questions = [
+    {
+        type:'list',
+        name: 'view-department',
+        message: 'View All Deparments',
+        choices: ['Sales', 'Engineering', 'Finance', 'Legal']
+    },
+    {
+        type:'list',
+        name: 'view-roles',
+        message: 'View All Roles',
+        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
+    },
+    {       
+        type:'list',
+        name: 'view-employees',
+        message: 'View All Employees',
+        choices: ['John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malia Brown', 'Sarah Lourd', 'Tom Allen']
+      
+    },
+    {
+        type:'input',
+        name: 'add-department',
+        message: 'What is the name of the department?'
+    },
+    
+]
+
+inquirer.prompt(questions)
+.then(answers  =>
+    {
+        console.log(answers);
+    });
