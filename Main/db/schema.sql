cf50000b1da87499fee5146c4bcfcaca371adb99
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-07-04 10:35:24 -0500

    added seeds file

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE employee_manager;
 \c employee_manager;
 
 CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL,
 );
 
 CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL
 );
 
 CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(30) UNIQUE NOT NULL,
     last_name VARCHAR(30) UNIQUE NOT NULL,
     role_id INTEGER NOT NULL,
